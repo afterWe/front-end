7cdff684f474455c22b26b81bcb4d6fb40916344
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-07-24 18:23:21 +0900

    ADD: Input, Button 생성

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from 'react';
+import { Input, Button } from '@class101/ui';
+import styled from 'styled-components';
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <LoginContainer>
+      <Input placeholder="아이디" />
+      <Input placeholder="비밀번호" />
+      <Button fill>로그인</Button>
+      <Button fill>회원가입 바로가기</Button>
+    </LoginContainer>
+  );
 };
 
 export default Login;
+
+const LoginContainer = styled.div`
+  display: column;
+  width: 50%;
+  height: 100vh;
+`;
