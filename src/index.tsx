a7b46c9d324482b1d314f78d687ca9788c778aee
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-08-03 20:38:25 +0900

    ADD: 공통 스타일 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './Styles/GlobalStyle';
+import { theme } from './Styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Router />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>
 );
