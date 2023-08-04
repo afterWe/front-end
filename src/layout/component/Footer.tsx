b3a37325ec309825ebb16d1802e477b44635cf13
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-08-04 11:07:59 +0900

    ADD : appLayout 추가 - MODIFY : styled 파일 분리

--- src/layout/component/Footer.tsx
+++ src/layout/component/Footer.tsx
@@ -1,7 +1,43 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import * as S from './style/Footer.style';
+import { Body1, Body2, Colors, TextButton } from '@class101/ui';
 
-const Footer: FC = () => {
-  return <div>Footer</div>;
+const MyComponent: FC = () => {
+  return (
+    <S.FooterContainer color={Colors.gray300}>
+      <S.FooterNavList>
+        {FOOTER_NAV_LIST.map(item => {
+          return (
+            <li key={item.id}>
+              <TextButton size="lg">{item.title}</TextButton>
+            </li>
+          );
+        })}
+      </S.FooterNavList>
+      <S.FooterContentWrap>
+        <Body1>Korea | &copy; RunBase 2023. All Rights Reserved</Body1>
+        <Body2>
+          이 웹사이트는 포트폴리오를 위해 제작되었으며, 본 사이트의 모든
+          컨텐츠와 제품들은 판매 목적이 아닌 예시 목적으로 사용되었습니다.
+        </Body2>
+        <S.InfoList>
+          <dt>전화 문의</dt>
+          <dd>010-8539-9495</dd>
+          <dt>이메일</dt>
+          <dd>some100696@gmail.com</dd>
+          <dt>호스팅서비스</dt>
+          <dd>AfterWe</dd>
+        </S.InfoList>
+      </S.FooterContentWrap>
+    </S.FooterContainer>
+  );
 };
 
-export default Footer;
+export default MyComponent;
+
+const FOOTER_NAV_LIST = [
+  { id: 1, title: '이용약관', Link: '' },
+  { id: 2, title: '결제방법', Link: '' },
+  { id: 3, title: '주문조회', Link: '' },
+  { id: 4, title: '문의하기', Link: '' }
+];
--- src/layout/component/Header.tsx
+++ src/layout/component/Header.tsx
@@ -1,7 +1,49 @@
 import React, { FC } from 'react';
+import * as S from './style/Header.style';
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <S.HeaderContainer>
+      <S.MainLogo>
+        <img src="/images/runbase_logo3.png" alt="런베이스 로고" />
+      </S.MainLogo>
+      <S.Nav>
+        <S.NavList>
+          {NAV_LIST.map(list => {
+            return (
+              <li key={list.id}>
+                <S.StyledBtn>{list.name}</S.StyledBtn>
+              </li>
+            );
+          })}
+        </S.NavList>
+      </S.Nav>
+      <S.MenuList>
+        {MENU_LIST.map(list => {
+          return (
+            <li key={list.id}>
+              <S.StyledBtn>{list.name}</S.StyledBtn>
+            </li>
+          );
+        })}
+      </S.MenuList>
+    </S.HeaderContainer>
+  );
 };
 
 export default Header;
+
+const NAV_LIST = [
+  { id: 1, name: 'NEW', router: '/' },
+  { id: 2, name: 'Clothing', router: '/' },
+  { id: 3, name: 'Shoes', router: '/' },
+  { id: 4, name: 'Items', router: '/' }
+];
+
+const MENU_LIST = [
+  { id: 1, name: '검색', router: '/' },
+  { id: 2, name: '로그인', router: '/' },
+  { id: 3, name: '회원가입', router: '/' },
+  { id: 4, name: '장바구니', router: '/' },
+  { id: 5, name: '마이페이지', router: '/' }
+];
