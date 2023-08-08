44b4f248d93ab1563a886404adcf686f18cddc76
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-08-08 21:12:34 +0900

    MODIFY: 로그인 레이아웃 수정

--- src/pages/Login/Login.style.ts
+++ src/pages/Login/Login.style.ts
@@ -1,36 +1,81 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
+import { Input, Button, TextButton, Icon, Colors } from '@class101/ui';
+
+interface Props {
+  width?: string;
+  margin?: string;
+}
 
 export const Login = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  height: 100vh;
-  width: 100%;
   align-items: center;
+  width: 100%;
+  height: 100vh;
 `;
 
-export const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  width: 60%;
-  height: 20vh;
+export const MainLogo = styled.h1`
+  position: fixed;
+  width: 13%;
+  top: 0;
+  left: 40px;
+
+  img {
+    width: 100%;
+    vertical-align: middle;
+  }
+`;
+
+export const LoginContainer = styled.div`
+  width: 30rem;
+`;
+
+export const StyledInput = styled(Input)<Props>`
+  width: ${({ width }) => width};
+  margin-bottom: 0.6rem;
+  border-color: ${Colors.gray400};
+
+  &:hover {
+    border-color: ${Colors.gray600};
+  }
 `;
 
 export const InputPasswordWrap = styled.div`
   position: relative;
 `;
 
-export const IconGroup = styled.div`
+export const StyledEye = styled(Icon.Eye)`
   position: absolute;
-  top: 15px;
-  right: 15px;
+  top: 0.8rem;
+  right: 1rem;
+
+  &:hover {
+    color: ${Colors.gray600};
+    cursor: pointer;
+  }
 `;
 
-export const ButtonContainer = styled.div`
+export const StyledButton = styled(Button)<Props>`
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  height: 20vh;
-  width: 60%;
+  justify-content: center;
+  margin-top: ${({ margin }) => margin};
+  background: ${Colors.gray400};
+  font-weight: 500;
+  font-size: 14px;
+
+  &:hover {
+    background: ${Colors.gray500};
+    border-color: ${Colors.gray500};
+  }
+`;
+
+export const TextButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledTextButton = styled(TextButton)<Props>`
+  display: flex;
+  justify-content: center;
+  margin-top: ${({ margin }) => margin};
 `;
