6a1f034bec0ee5d6ca3d3fcf8269c2d7c6129c28
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-08-09 20:35:59 +0900

    수정중

---
+++ src/pages/Cart/Cart.style.ts
@@ -0,0 +1,77 @@
+import { styled } from 'styled-components';
+
+export const Cart = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const CartListContainer = styled.div`
+  border: 1px solid black;
+  width: 60%;
+`;
+
+export const FreeShipServiceWrap = styled.div`
+  border: 1px solid 0.5;
+  padding: 1rem;
+`;
+
+export const FreeShipServiceTitle = styled.div``;
+
+export const FreeShipServiceContent = styled.div``;
+
+export const ReturnAndRefundWrap = styled.div`
+  border: 1px solid black;
+  padding: 1rem;
+`;
+export const ReturnAndRefundTitle = styled.div``;
+
+export const ReturnAndRefundContent = styled.div``;
+
+export const CartListWrap = styled.div`
+  border: 1px solid black;
+`;
+
+export const CartListTitle = styled.h1`
+  font-weight: 500;
+  padding: 1rem;
+`;
+
+export const CartListGroup = styled.div`
+  display: flex;
+  border: 1px solid black;
+`;
+
+export const ImageBox = styled.image`
+  border: 1px solid black;
+`;
+
+export const CartListDetailBox = styled.div``;
+
+export const ItemInfoAndDelete = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+`;
+
+export const ItemInfoName = styled.span``;
+
+export const ItemInfoDetail = styled.span``;
+
+export const SelectAndItemPrice = styled.div`
+  border: 1px solid black;
+`;
+
+export const ItemPrice = styled.div``;
+
+export const OrderListContainer = styled.div`
+  border: 1px solid black;
+  width: 40%;
+`;
+
+export const OrderListTitle = styled.div``;
+
+export const ProductPriceBox = styled.div``;
+
+export const ShippingFee = styled.div``;
+
+export const TotalPrice = styled.div``;
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,63 @@
 import React, { FC } from 'react';
+import { Button, Divider, Icon, Select } from '@class101/ui';
+import * as S from './Cart.style';
 
 const Cart: FC = () => {
-  return <div>Cart</div>;
+  return (
+    <S.Cart>
+      <S.CartListContainer>
+        <S.FreeShipServiceWrap>
+          <S.FreeShipServiceTitle>무료배송서비스</S.FreeShipServiceTitle>
+          <S.FreeShipServiceContent>
+            배송서비스를 즐겨봐
+          </S.FreeShipServiceContent>
+        </S.FreeShipServiceWrap>
+        <S.ReturnAndRefundWrap>
+          <S.ReturnAndRefundTitle>반품 및 환불안내</S.ReturnAndRefundTitle>
+          <S.ReturnAndRefundContent>자세히보기</S.ReturnAndRefundContent>
+        </S.ReturnAndRefundWrap>
+        <S.CartListWrap>
+          <S.CartListTitle>장바구니</S.CartListTitle>
+          <S.CartListGroup>
+            <S.ImageBox>이미지</S.ImageBox>
+            <S.CartListDetailBox>
+              <S.ItemInfoAndDelete>
+                <S.ItemInfoName>나이키 에어 포스 1 쉐도우 복서</S.ItemInfoName>
+                <Icon.Trash />
+                <S.ItemInfoDetail>남성 신발 240/white</S.ItemInfoDetail>
+              </S.ItemInfoAndDelete>
+              <S.SelectAndItemPrice>
+                <Select
+                  value=""
+                  placeholder="1"
+                  options={['1', '2', '3', '4', '5']}
+                  size="sm"
+                />
+                <S.ItemPrice>159,000</S.ItemPrice>
+              </S.SelectAndItemPrice>
+            </S.CartListDetailBox>
+          </S.CartListGroup>
+          <Divider />
+        </S.CartListWrap>
+      </S.CartListContainer>
+      <S.OrderListContainer>
+        <S.OrderListTitle>주문 내역</S.OrderListTitle>
+        <S.ProductPriceBox>
+          <span>상품금액</span>
+          <span>data</span>
+        </S.ProductPriceBox>
+        <S.ShippingFee>
+          <span>배송비</span>
+          <span>0원</span>
+        </S.ShippingFee>
+        <S.TotalPrice>
+          <span>총 결제 금액</span>
+          <span>data</span>
+        </S.TotalPrice>
+        <Button>결제 하기</Button>
+      </S.OrderListContainer>
+    </S.Cart>
+  );
 };
 
 export default Cart;
