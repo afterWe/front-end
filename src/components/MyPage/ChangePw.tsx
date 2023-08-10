87f6698f31cbd7098cf63ae2412f2849da22ea0e
zunyange <hiillun00553300@gmail.com>
2023-08-10 22:52:05 +0900

    ADD : Address Edit Modal

--- src/components/MyPage/ChangePw.tsx
+++ src/components/MyPage/ChangePw.tsx
@@ -14,12 +14,12 @@ const ChangePw: FC = () => {
             <S.StyledInput
               type="password"
               placeholder="새로운 비밀번호를 입력해주세요."
-              errorMessage="알파벳 소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다."
+              // errorMessage="알파벳 소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다."
             />
             <S.StyledInput
               type="password"
               placeholder="새로운 비밀번호를 한번 더 입력해주세요."
-              errorMessage="비밀번호가 일치하지 않습니다."
+              // errorMessage="비밀번호가 일치하지 않습니다."
             />
           </S.InputPasswordWrap>
           <S.StyledButton fill size={ButtonSize.LARGE} margin="2rem">
--- src/components/MyPage/PersonalInfo.tsx
+++ src/components/MyPage/PersonalInfo.tsx
@@ -1,9 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { ButtonSize } from '@class101/ui';
+import { StyledInput } from '../../Styles/common.style';
 import * as S from './PersonalInfo.style';
+import Modal from '../Modal/Modal';
 
 const PersonalInfo: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleAlert = (value: string) => {
-    console.log(value);
+    // console.log(value);
+  };
+
+  const showModal = () => {
+    setIsOpen(true);
   };
 
   return (
@@ -18,14 +27,69 @@ const PersonalInfo: FC = () => {
             <S.StyledInput label="전화번호 *" placeholder="010-1234-5678" />
             <S.StyledInput label="아이디 *" placeholder="dlthadl2@naver.com" />
             <S.ButtonBox>
-              <S.StyledButton>수정</S.StyledButton>
-              <S.StyledButton>취소</S.StyledButton>
+              <S.SaveButton>수정</S.SaveButton>
+              <S.CancelButton>취소</S.CancelButton>
             </S.ButtonBox>
           </S.InputBox>
           <S.ShippingAdderssBox>
             <S.ShippingTitle>
               <S.AddressList>배송지 목록</S.AddressList>
-              <S.AddAddress>배송지 추가</S.AddAddress>
+              {/* {isOpen && (
+                <Modal
+                  opener={<div>클릭!</div>}
+                  contents="장바구니에 담겼습니다"
+                  successText="확인"
+                />
+              )} */}
+              {isOpen && (
+                <Modal
+                  opener={<div>배송지 추가</div>}
+                  contents={
+                    <S.AddressEditContainer>
+                      <S.EditTitle>배송지 추가(수정)</S.EditTitle>
+                      <S.EditBox>
+                        <StyledInput
+                          margin="0 0 0.5rem 0"
+                          label="배송지이름 *"
+                          // errorMessage='배송지 이름을 입력해주세요.'
+                        />
+                        <StyledInput
+                          margin="0 0 0.5rem 0"
+                          label="수령인 *"
+                          // errorMessage='수령인 이름을 입력해주세요.'
+                        />
+                        <StyledInput
+                          margin="0 0 0.5rem 0"
+                          label="휴대폰번호 *"
+                          // errorMessage='휴대폰번호를 입력해주세요.'
+                        />
+                        <S.StyledControlGroup>
+                          <StyledInput
+                            margin="0 0 0.5rem 0"
+                            disabled
+                            label="우편번호"
+                          />
+                          <S.StyledButton size={ButtonSize.LARGE}>
+                            주소검색
+                          </S.StyledButton>
+                        </S.StyledControlGroup>
+                        <StyledInput
+                          margin="0 0 0.5rem 0"
+                          label="도로명주소 *"
+                          // errorMessage='주소를 검색해주세요.'
+                        />
+                        <StyledInput
+                          margin="0 0 0.5rem 0"
+                          label="상세주소 *"
+                          // errorMessage='상세주소를 입력해주세요.'
+                        />
+                      </S.EditBox>
+                    </S.AddressEditContainer>
+                  }
+                  successText="저장"
+                />
+              )}
+              <S.AddAddress onClick={showModal}>배송지 추가</S.AddAddress>
             </S.ShippingTitle>
             <S.ShippingAddressList>
               <S.StyledRadioButtonGroup
