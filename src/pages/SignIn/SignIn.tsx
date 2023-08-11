b29c662600e970c0b40e913933ec017f7593d96f
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-08-11 10:45:34 +0900

    MODIFY: 피드백 적용 완료
    - MODIFY: 피드백 반영 중
    
    - MODIFY: PageContainer 적용, 로고 위치 변경

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,63 +1,74 @@
 import React, { FC } from 'react';
-import { ButtonSize, Colors } from '@class101/ui';
+import { ButtonSize } from '@class101/ui';
 import * as S from './SignIn.style';
-import { Link } from 'react-router-dom';
+import { StyledButton, StyledInput } from '../../Styles/common.style';
+import { theme } from '../../Styles/theme';
 
 const SignIn: FC = () => {
   return (
     <S.SignIn>
-      <Link to="/">
-        <S.MainLogo>
-          <img src="/images/runbase_logo3.png" alt="런베이스 로고" />
-        </S.MainLogo>
-      </Link>
+      <S.MainLogo>
+        <img src="/images/runbase_logo3.png" alt="런베이스 로고" />
+      </S.MainLogo>
       <S.SignInContainer>
-        <S.StyledInput
+        <StyledInput
           placeholder="이름"
+          margin="0 0 0.4rem 0"
           //errorMessage="정확한 이름을 입력해주세요."
         />
         <S.InputButtonGroup>
-          <S.StyledInput
+          <StyledInput
             type="email"
             placeholder="아이디(이메일)"
             width="97%"
-            //errorMessage="@, 마침표(.)를 포함해야만 합니다."
+            margin="0 0 0.4rem 0"
+            //errorMessage={ERROR_MSG[0].msg}
+            //allowMessage="사용 가능한 아이디입니다."
           />
-          <S.StyledButton type="submit" size={ButtonSize.LARGE}>
+          <StyledButton type="submit" size={ButtonSize.LARGE} isMargin={false}>
             중복확인
-          </S.StyledButton>
+          </StyledButton>
         </S.InputButtonGroup>
         <S.InputEyeGroup>
-          <S.StyledInput
+          <StyledInput
             type="password"
             placeholder="비밀번호"
+            margin="0 0 0.4rem 0"
             //errorMessage="알파벳 소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다."
           />
-          <S.StyledEye fillColor={Colors.gray400} />
+          <S.StyledEye fillColor={theme.gray} />
         </S.InputEyeGroup>
         <S.InputEyeGroup>
-          <S.StyledInput
+          <StyledInput
             type="password"
             placeholder="비밀번호 확인"
+            margin="0 0 0.4rem 0"
             //errorMessage="비밀번호가 일치하지 않습니다."
           />
-          <S.StyledEye fillColor={Colors.gray400} />
+          <S.StyledEye fillColor={theme.gray} />
         </S.InputEyeGroup>
-        <S.StyledInput
+        <StyledInput
           placeholder="전화번호"
+          margin="0 0 1rem 0"
           //errorMessage="전화번호를 올바르게 입력해주세요."
         />
-        <S.StyledButton
+        <StyledButton
           fill
           type="submit"
           size={ButtonSize.LARGE}
-          margin="2rem"
+          isMargin={false}
         >
           회원가입
-        </S.StyledButton>
+        </StyledButton>
       </S.SignInContainer>
     </S.SignIn>
   );
 };
 
 export default SignIn;
+
+const ERROR_MSG = [
+  { id: 1, error: 'validate', msg: '@, 마침표(.)를 포함해야만 합니다.' },
+  { id: 2, error: 'confirm', msg: '중복 확인을 해주세요.' },
+  { id: 3, error: 'duplicate', msg: '이미 존재하는 아이디입니다.' }
+];
