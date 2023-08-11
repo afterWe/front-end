9220a504600ae379a3d4454d2fc1164b40486757
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-08-11 10:45:34 +0900

    MODIFY: 피드백 적용 완료
    - MODIFY: 피드백 반영 중
    
    - MODIFY: PageContainer 적용, 로고 위치 변경

--- src/pages/SignIn/SignIn.style.ts
+++ src/pages/SignIn/SignIn.style.ts
@@ -1,72 +1,46 @@
 import styled from 'styled-components';
-import { Input, Button, Icon, Colors } from '@class101/ui';
+import { EyeOffIcon } from '@class101/ui';
+import { PageContainer } from '../../Styles/common.style';
 
-interface Props {
-  width?: string;
-  margin?: string;
-}
-
-export const SignIn = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+export const SignIn = styled(PageContainer)`
+  ${({ theme }) => theme.flexBox('center', 'center', 'column')};
   height: 100vh;
+`;
+
+export const MainLogo = styled.h1`
+  margin-bottom: 2rem;
+  text-align: center;
+
+  img {
+    width: 30%;
+  }
 `;
 
 export const SignInContainer = styled.form`
   width: 30rem;
 `;
 
-export const MainLogo = styled.h1`
-  position: fixed;
-  top: 0;
-  left: 40px;
-  width: 13%;
-
-  img {
-    width: 100%;
-  }
-`;
-
-export const StyledInput = styled(Input)<Props>`
-  width: ${({ width }) => width};
-  margin-bottom: 0.4rem;
-  border-color: ${Colors.gray400};
-
-  &:hover {
-    border-color: ${Colors.gray600};
-  }
-`;
-
 export const InputButtonGroup = styled.form`
-  display: flex;
-  justify-content: space-between;
+  ${({ theme }) => theme.flexBox('space-between', 'undefined', 'undefined')};
 `;
 
 export const InputEyeGroup = styled.span`
-  position: relative;
+  ${({ theme }) =>
+    theme.positionBox(
+      'relative',
+      'undefined',
+      'undefined',
+      'undefined',
+      'undefined'
+    )};
 `;
 
-export const StyledEye = styled(Icon.Eye)`
-  position: absolute;
-  top: 0.7rem;
-  right: 1rem;
+export const StyledEye = styled(EyeOffIcon)`
+  ${({ theme }) =>
+    theme.positionBox('absolute', '0.7rem', '1rem', 'undefined', 'undefined')}
   z-index: 9;
 
   &:hover {
     cursor: pointer;
-  }
-`;
-
-export const StyledButton = styled(Button)<Props>`
-  margin-top: ${({ margin }) => margin};
-  background: ${Colors.gray400};
-  font-weight: 500;
-  font-size: 14px;
-
-  &:hover {
-    background: ${Colors.gray500};
-    border-color: ${Colors.gray500};
   }
 `;
