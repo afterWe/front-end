19e83aa9447c36c14e3959016492464387df1bb7
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-08-13 23:40:57 +0900

    MODIFY: 배송지추가(수정) 레이아웃 수정

--- src/components/Address/AddressEdit.style.ts
+++ src/components/Address/AddressEdit.style.ts
@@ -1,33 +1,8 @@
 import styled from 'styled-components';
-import { Input, Colors, ControlGroup, Button, Divider } from '@class101/ui';
+import { ControlGroup, Button } from '@class101/ui';
 
-export const AddressEdit = styled.div`
-  margin-top: 10rem;
+export const AddressEditContainer = styled.div`
   width: 100%;
-`;
-
-export const HeadContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const HeadTitle = styled.h3`
-  font-size: 2rem;
-  margin: 0 auto;
-`;
-
-export const StyledDivider = styled(Divider)`
-  padding: 2rem 0rem;
-`;
-
-export const InputContainer = styled.div``;
-
-export const StyledInput = styled(Input)`
-  margin-bottom: 0.6rem;
-  border-color: ${Colors.gray400};
-  &:hover {
-    border-color: ${Colors.gray600};
-  }
 `;
 
 export const StyledControlGroup = styled(ControlGroup)`
@@ -35,7 +10,8 @@ export const StyledControlGroup = styled(ControlGroup)`
 `;
 
 export const StyledButton = styled(Button)`
-  margin: 0.3rem 5rem 0rem 5rem;
+  margin: 0.5rem 5rem 0 5rem;
+  border-radius: 0.5rem;
   font-size: 1rem;
   font-weight: 500;
 `;
