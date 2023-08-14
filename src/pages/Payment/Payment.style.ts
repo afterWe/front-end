d5db544c28c6076439f417a396ad000be08dec7c
minngaeng <minngaeng@gmail.com>
2023-08-14 11:00:04 +0900

    Modified:feedback

--- src/pages/Payment/Payment.style.ts
+++ src/pages/Payment/Payment.style.ts
@@ -1,48 +1,37 @@
 import { styled } from 'styled-components';
-import { Button } from '@class101/ui';
+import { Button, Colors, Divider } from '@class101/ui';
+import { PageContainer } from '../../Styles/common.style';
 
-export const Payment = styled.div`
+export const Payment = styled(PageContainer)`
   box-sizing: border-box;
-  width: 80%;
-  margin: 100px auto;
-  /* padding-top: 80px;
-  padding-bottom: 100px;
-  padding-left: 102px;
-  padding-right: 102px; */
-  /* display: flex;
-  flex-direction: column;
-  align-items: center; */
 `;
 
-export const Header = styled.h2``;
-
-export const TitleWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const TitleBox = styled.div`
+  ${({ theme }) => theme.flexBox('', 'center', 'column')}
 `;
 
 export const Title = styled.h2`
-  /* font-size: 20px; */
-  font-weight: 600;
+  font-size: 2rem;
 `;
 
-export const BodyBodyWrap = styled.div``;
-
-export const BodyWrap = styled.div`
-  /* border: 1px solid black; */
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 3.5rem 0;
+export const StyledDivider = styled(Divider)`
+  padding: 2rem 0rem;
 `;
 
-export const DelieveryGroup = styled.div`
-  /* border: 1px solid black; */
-  width: 48%;
-
-  /* padding-right: 10px; */
+export const PaymentInfoContainer = styled.div`
+  margin: 2rem 0;
+  ${({ theme }) => theme.flexBox('space-between', '', 'row')}
 `;
+
+export const DelieveryWrap = styled.div`
+  width: 47%;
+`;
+
+export const DelieveryTitle = styled.h3`
+  font-size: 1.5rem;
+`;
+
+// -------------정인님 컴포넌트 스타일 들어올 부분 시작---------
 
 export const DelieveryGroupTopBox = styled.div`
   display: flex;
@@ -56,68 +45,7 @@ export const shippingList = styled.u`
   color: #909090;
 `;
 
-export const DelieveryTitle = styled.div`
-  margin: 30px 0;
-
-  font-size: 18px;
-`;
-
 export const InputBox = styled.div``;
-
-export const PaymentInfoGroup = styled.div`
-  width: 35%;
-
-  /* border: 1px solid black; */
-`;
-
-export const PaymentTitle = styled(DelieveryTitle)`
-  /* padding: 0 0.8rem; */
-`;
-
-export const PaymentDetailBox = styled.div`
-  /* padding: 0 0.8rem; */
-`;
-
-export const PaymentDetailInfoBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 2rem 0;
-`;
-
-export const PaymentDetailImgBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  /* justify-content: space-between; */
-  padding: 1rem 0;
-`;
-
-export const OrderProductImg = styled.img`
-  width: 38%;
-  height: 8rem;
-  background-color: #f5f5f5;
-  border: none;
-`;
-
-export const OrderProductDetail = styled.div`
-  padding-left: 1rem;
-  line-height: 2rem;
-`;
-
-export const OrderProductDetailTitle = styled.div`
-  font-size: 15px;
-  font-weight: 700;
-`;
-
-export const OrderProductSubDetail = styled.div`
-  font-size: 13px;
-  font-weight: 500;
-`;
-
-export const StyledButton = styled(Button)`
-  margin-left: 2.2rem;
-  height: 48px;
-`;
 
 export const PostCodeBox = styled.div`
   display: flex;
@@ -125,15 +53,70 @@ export const PostCodeBox = styled.div`
   align-items: flex-end;
 `;
 
-export const GoToPay = styled.div`
-  border: 1px solid black;
+// -------------------끝---------------------------
+
+export const PaymentInfoWrap = styled.div`
+  width: 43%;
+`;
+
+export const PaymentTitle = styled.h3`
+  font-size: 1.5rem;
+`;
+
+export const PaymentDetailInfoBox = styled.ul`
+  margin: 2rem 0;
+  font-size: 1.1rem;
+  ${({ theme }) => theme.flexBox('space-between', '', 'row')}
+`;
+
+export const PaymentOrderListWrap = styled.div`
+  height: 330px;
+  overflow-y: auto;
+`;
+
+export const PaymentDetailImgGroup = styled.div`
+  margin: 2rem 0;
+  ${({ theme }) => theme.flexBox('flex-start', '', 'row')}
+`;
+
+export const OrderImageBox = styled.div`
+  width: 10rem;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const OrderProductDetail = styled.ul`
+  margin-left: 3rem;
+  font-size: 1rem;
+  ${({ theme }) => theme.flexBox('space-around', '', 'column')}
+`;
+
+export const OrderProductSubDetail = styled.ul`
+  line-height: 1.5rem;
+  font-size: 0.8rem;
+`;
+
+export const StyledButton = styled(Button)`
+  margin: 3.5rem 0;
+  height: 4rem;
+  font-size: 1rem;
+  background-color: ${Colors.black};
+  border-radius: 2rem;
+  color: ${Colors.white};
+  font-weight: 500;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.darkGray};
+  }
 `;
 
 export const PaymentMethod = styled(DelieveryTitle)`
-  margin: 3.5rem 0;
-  line-height: 2rem;
+  margin: 3rem 0;
 `;
 
-export const PaymentMethodTitle = styled.div`
+export const PaymentMethodTitle = styled.h3`
   margin: 1.5rem 0;
 `;
