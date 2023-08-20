0b891133e3f935b264b320a304a7c4de02f9cd91
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-08-20 23:19:21 +0900

    ADD: 배송지추가 모달

--- src/components/Address/AddressList.style.ts
+++ src/components/Address/AddressList.style.ts
@@ -1,33 +1,33 @@
 import styled from 'styled-components';
-import { RadioButton, TextButton } from '@class101/ui';
+import { TextButton } from '@class101/ui';
 
 export const AddressList = styled.div`
-  width: 30rem;
-  margin: 0 auto;
+  ${({ theme }) => theme.flexBox('space-between', 'center', 'column')}
 `;
 
 export const AddressAddBox = styled.div`
   ${({ theme }) => theme.flexBox('right', undefined, undefined)}
-  margin-bottom: 5rem;
-`;
-
-export const AddressListContainer = styled.div`
   width: 80%;
-  margin: 0 auto;
-`;
-
-export const StyledRadioButton = styled(RadioButton)`
-  align-items: flex-start;
-  &:nth-child(1) div {
-    margin-top: -1px;
-  }
-`;
-
-export const StyledRadioButtonBox = styled.div`
-  ${({ theme }) => theme.flexBox('space-between', undefined, undefined)}
+  margin-bottom: 2rem;
 `;
 
 export const StyledTextButton = styled(TextButton)`
   margin-left: 0.5rem;
   text-decoration-line: underline;
+`;
+
+export const AddressTitleBox = styled.div`
+  ${({ theme }) => theme.flexBox('space-between', undefined, undefined)}
+  width: 80%;
+  margin-bottom: 2rem;
+`;
+
+export const AddressListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`;
+
+export const StyledRadioBtn = styled.div`
+  ${({ theme }) => theme.flexBox('space-between', 'baseline', undefined)}
 `;
