712e6033a10a2be9023a6371c94116dca138ff86
zunyange <hiillun00553300@gmail.com>
2023-09-14 16:04:46 +0900

    FIX : Header Function with token

--- src/layout/component/Header.tsx
+++ src/layout/component/Header.tsx
@@ -1,9 +1,60 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Colors } from '@class101/ui';
 import * as S from './style/Header.style';
 
 const Header: FC = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+
+  const conditionLink = (name: string) => {
+    if (name === '로그아웃') {
+      if (token) {
+        localStorage.removeItem('token');
+        navigate('/myPage');
+      }
+    }
+
+    if (name === '장바구니') {
+      if (token) {
+        navigate('/cart');
+      } else if (!token) {
+        alert('로그인 해주세요.');
+        navigate('/login');
+      }
+    }
+
+    if (name === '마이페이지') {
+      if (token) {
+        navigate('/mypage');
+      } else if (!token) {
+        alert('로그인 해주세요.');
+        navigate('/login');
+      }
+    }
+  };
+
+  const MENU_LIST = [
+    { id: 1, name: '검색', router: '/' },
+    {
+      id: 2,
+      name: token ? '로그아웃' : '로그인',
+      router: token ? '/' : '/login',
+      onClick: () => conditionLink(token ? '로그아웃' : '로그인')
+    },
+    { id: 3, name: '회원가입', router: '/sign-in' },
+    {
+      id: 4,
+      name: '장바구니',
+      router: token ? '/cart' : '/login'
+    },
+    {
+      id: 5,
+      name: '마이페이지',
+      router: token ? '/mypage' : '/login'
+    }
+  ];
+
   return (
     <S.HeaderContainer>
       <S.MainLogo>
@@ -18,9 +69,7 @@ const Header: FC = () => {
           {NAV_LIST.map(list => {
             return (
               <li key={list.id}>
-                <Link to={list.router}>
-                  <S.StyledBtn color={Colors.blue700}>{list.name}</S.StyledBtn>
-                </Link>
+                <S.StyledBtn color={Colors.blue700}>{list.name}</S.StyledBtn>
               </li>
             );
           })}
@@ -30,9 +79,17 @@ const Header: FC = () => {
         {MENU_LIST.map(list => {
           return (
             <li key={list.id}>
-              <Link to={list.router}>
-                <S.StyledBtn color={Colors.blue700}>{list.name}</S.StyledBtn>
-              </Link>
+              {list.router ? (
+                <Link to={list.router}>
+                  <S.StyledBtn color={Colors.blue700} onClick={list.onClick}>
+                    {list.name}
+                  </S.StyledBtn>
+                </Link>
+              ) : (
+                <S.StyledBtn color={Colors.blue700} onClick={list.onClick}>
+                  {list.name}
+                </S.StyledBtn>
+              )}
             </li>
           );
         })}
@@ -48,12 +105,4 @@ const NAV_LIST = [
   { id: 2, name: 'Clothing', router: '/product-list/clothing' },
   { id: 3, name: 'Shoes', router: '/product-list/shoes' },
   { id: 4, name: 'Items', router: '/product-list/items' }
-];
-
-const MENU_LIST = [
-  { id: 1, name: '검색', router: '/' },
-  { id: 2, name: '로그인', router: '/login' },
-  { id: 3, name: '회원가입', router: '/sign-in' },
-  { id: 4, name: '장바구니', router: '/cart' },
-  { id: 5, name: '마이페이지', router: '/mypage' }
 ];
