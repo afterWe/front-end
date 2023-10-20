4794dbbe655f242b989cd487b75631b5e96a1875
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-10-20 14:07:47 +0900

    ADD: implement fetch data -ADD: category link -MODIFY: header link -implement add cart data function -MODIFY: 주소 수정 -ADD: category link -MODIFY: header link -implement add cart data function -MODIFY: 주소 수정 -ADD: 정렬 구현 중 -ADD: filter 구현 중 -MODIFY: fetch to axios -MODIFY: change fetch url -ADD: 정렬 기능 추가 -MODIFY: api 수정

---
+++ src/pages/ProductList/ProductAside.style.ts
@@ -0,0 +1,80 @@
+import { styled } from 'styled-components';
+import { SelectColorProps } from '../../types/components';
+import { Colors, Button } from '@class101/ui';
+
+export const ProductAside = styled.aside`
+  position: sticky;
+  top: 0;
+  margin: 0 20px 0 45px;
+  width: 18%;
+  height: 40rem;
+  ${({ theme }) => theme.flexBox('space-around', '', 'column')}
+`;
+
+export const ListTitle = styled.h3`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const SizeCategory = styled.h5`
+  margin: 1.5rem 0;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const SizeList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 3.5rem);
+  grid-template-rows: repeat(3, 2.5rem);
+  grid-gap: 0.5rem;
+  justify-items: end;
+`;
+
+export const SizeButton = styled(Button)`
+  font-size: 16px;
+`;
+
+export const ColorCategory = styled(SizeCategory)``;
+
+export const ColorList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 4rem);
+  grid-template-rows: repeat(2, 2.5rem);
+  grid-gap: 0.3rem;
+`;
+
+export const ColorBtn = styled.button<SelectColorProps>`
+  background-color: ${props => props.color};
+  width: 2rem;
+  height: 2rem;
+  border: ${props =>
+    props.color === Colors.white ? `0.1rem solid ${Colors.black}` : 'none'};
+  border-radius: 50%;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const PriceCategory = styled(SizeCategory)``;
+
+export const PriceRangeWrap = styled.div`
+  width: 13rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const StyledLabel = styled.label`
+  ${({ theme }) => theme.flexBox('', 'center', '')}
+  cursor: pointer;
+
+  &:hover {
+    color: ${Colors.gray500};
+  }
+
+  input[type='checkbox'] {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    margin-right: 0.7rem;
+  }
+`;
