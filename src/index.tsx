3bafbec0c407a3010a4432ede460b81f149a3da5
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-11-02 14:07:46 +0900

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './Styles/GlobalStyle';
 import { theme } from './Styles/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <Router />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
